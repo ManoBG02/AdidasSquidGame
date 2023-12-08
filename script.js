90215fce733dfb7712e2158951e051e02ebb2255
Bruno Oliveira <nuk3bgleal02@gmail.com>
2023-12-07 23:07:30 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Event Listeners for Navigation
+document.querySelector(".column-header-3").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change3");
+});
+
+document.querySelector(".column-header-2").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change2");
+});
+
+document.querySelector(".column-header-1").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change1");
+});
+
+document.querySelector(".mobile-menu-icon").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+
